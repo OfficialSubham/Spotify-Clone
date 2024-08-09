e7d9389e767bfaae2ceeb604a9410cae2c1c6d1f
Subham <subhamoffi@gmail.com>
2024-08-09 21:27:37 +0530

    timer is working with js

--- JavaScript Files/main.js
+++ JavaScript Files/main.js
@@ -27,31 +27,33 @@ async function getSongs() {
 
 async function main() {
     let song = await getSongs();
-    console.log(song);
     let ul = document.createElement("ul")
     let list = '';
     song.forEach((song) => {
-        let eachSong = `<li data-song-id = ${song}>${songNameGiver(song)}</li>`
+        let className = removeLinkPart(song)
+        let eachSong = `<li data-song-id = ${song} class = "js-${className} js-song">${songNameGiver(song)}</li>`
         list += eachSong;
     })
     ul.innerHTML = list;
     document.querySelector(".js-list")
         .innerHTML = list
-    currentTrack.src = song[3]
-    //setInterval(() => {
-        //console.log(currentTrack.currentTime);
-    //}, 2000)
+    currentTrack.src = song[8]
+    let timeInterval;
     let songName = songNameGiver(currentTrack.src)
     document.querySelector(".js-song-name")
         .innerText = songName
     document.querySelector(".js-play-pause-button")
         .addEventListener("click", () => {
             if(currentTrack.paused) {
-                currentTrack.play();
                 document.querySelector(".js-play-pause-button")
                     .classList.remove("fa-play")
                 document.querySelector(".js-play-pause-button")
                     .classList.add("fa-pause")
+                    currentTrack.play();
+                songTime(currentTrack.duration)
+                 timeInterval = setInterval(() => {
+                    songTimer(currentTrack.currentTime);
+                }, 1000)
             }
             else {
                 currentTrack.pause();
@@ -59,13 +61,14 @@ async function main() {
                     .classList.remove("fa-pause")
                 document.querySelector(".js-play-pause-button")
                     .classList.add("fa-play")
+                songTime(currentTrack.duration)
+                clearInterval(timeInterval);
             }
         })
 }
 
 function songNameGiver(song) {
     let name = song.split("/Songs%20Data/")[1]
-    console.log(name);
     let newName = name.replaceAll(/[%20, %2, (), _,]/g, " ")
     return newName
 }
@@ -101,6 +104,83 @@ function visibleHamburgerMenu() {
         })
 }   
 
-let ulList = document.querySelector(".js-list")
+playSong();
 
-console.log(ulList.innerHTML);
+function removeLinkPart(fullLink) {
+    let newLink = fullLink.split("/Songs%20Data/")[1];
+    let removeSpace = newLink.replaceAll("%20", "-")
+    newLink = removeSpace.replace(".mp3", "")
+    return newLink;
+}
+
+function playSong() {
+    let ulList = document.querySelector(".js-list")
+    let list = ulList.getElementsByTagName("li")
+    console.log(list);
+
+}
+
+document.querySelectorAll("ul")
+    .forEach((song) => {
+        console.log("hi");
+        document.querySelectorAll(".js-song")
+            .forEach(()=> {
+                console.log("hell nawh");
+            })
+        console.log(song);
+        song.addEventListener('click', () => {
+            console.log(song);
+        })
+});
+
+function songTime(currentSong) {
+    let time = currentSong
+    let mainMinute = Math.floor(Number(time) / 60);
+    let mainSecond = Math.floor(Number(time) % 60);
+    let minute;
+    let second;
+    if(mainMinute < 10) {
+        minute = "0" + mainMinute;
+    }
+    else {
+        minute = mainMinute;
+    }
+    if(mainSecond < 10) {
+        second = "0" + mainSecond;
+        console.log(second);
+    }
+    else {
+        second = mainSecond
+    }
+
+    document.querySelector(".js-minute")
+        .innerText = minute;
+    document.querySelector(".js-second")
+        .innerText = second;
+}
+
+function songTimer(time) {
+    let mainMinute = Math.floor(time / 60);
+    let mainSecond = Math.floor(time % 60);
+    let minute;
+    let second;
+
+    if(mainMinute < 10) {
+        minute = "0" + mainMinute;
+    }
+    else {
+        minute = mainMinute;
+    }
+    if(mainSecond < 10) {
+        second = "0" + mainSecond;
+    }
+    else {
+        second = mainSecond
+    }
+
+    document.querySelector(".js-minute-timer")
+        .innerText = minute;
+    document.querySelector(".js-second-timer")
+        .innerText = second;
+
+}
